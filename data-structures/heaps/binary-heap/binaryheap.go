11b6ead8638a8a751f76803771480c0d1cf784de
Ray Guo <ray_g@live.com>
2017-05-03 01:14:55 +0800

    add binary heap

---
+++ data-structures/heaps/binary-heap/binaryheap.go
@@ -0,0 +1,79 @@
+package heap
+
+type Item interface {
+	Less(than interface{}) bool
+}
+
+type Heap struct {
+	data []Item
+	min  bool
+}
+
+func New(isMin bool) *Heap {
+	return &Heap{
+		data: []Item{nil},
+		min:  isMin,
+	}
+}
+
+func (h *Heap) IsEmpty() bool {
+	return len(h.data) == 1
+}
+
+func (h *Heap) Len() int {
+	return len(h.data) - 1
+}
+
+func (h *Heap) Less(i, j int) bool {
+	if h.min {
+		return h.data[i].Less(h.data[j])
+	}
+	return h.data[j].Less(h.data[i])
+
+}
+
+func (h *Heap) Swap(i, j int) {
+	h.data[i], h.data[j] = h.data[j], h.data[i]
+}
+
+func (h *Heap) Swim(k int) {
+	for k > 1 && h.Less(k, k/2) {
+		h.Swap(k/2, k)
+		k = k / 2
+	}
+}
+
+func (h *Heap) Sink(k int) {
+	n := h.Len()
+	for 2*k <= n {
+		j := 2 * k
+		if j < n && h.Less(j+1, j) {
+			j++
+		}
+		if !h.Less(j, k) {
+			break
+		}
+		h.Swap(k, j)
+		k = j
+	}
+}
+
+func (h *Heap) Insert(n Item) {
+	h.data = append(h.data, n)
+	h.Swim(h.Len())
+}
+
+func (h *Heap) Extract() (n Item) {
+	if h.IsEmpty() {
+		return
+	}
+
+	n = h.data[1]
+	h.Swap(1, h.Len())
+	h.data = h.data[0:h.Len()]
+
+	if h.Len() > 1 {
+		h.Sink(1)
+	}
+	return
+}
---
+++ data-structures/heaps/binary-heap/binaryheap_test.go
@@ -0,0 +1,57 @@
+package heap
+
+import "testing"
+
+type Int int
+
+func (x Int) Less(than interface{}) bool {
+	return x < than.(Int)
+}
+
+func TestMinHeap(t *testing.T) {
+	h := New(true)
+
+	nums := []int{8, 7, 6, 3, 1, 0, 0, 0, 9, 0, 2, 4, 9, 5}
+	for _, n := range nums {
+		h.Insert(Int(n))
+	}
+
+	ordered := make([]Int, 0)
+	for h.Len() > 0 {
+		ordered = append(ordered, h.Extract().(Int))
+	}
+
+	if nil != h.Extract() {
+		t.Error()
+	}
+
+	for i := 0; i < len(ordered)-2; i++ {
+		if ordered[i] > ordered[i+1] {
+			t.Errorf("!Ordered. %v", ordered)
+		}
+	}
+}
+
+func TestMaxHeap(t *testing.T) {
+	h := New(false)
+
+	nums := []int{8, 7, 6, 3, 1, 9, 9, 9, 9, 0, 2, 4, 9, 5}
+	for _, n := range nums {
+		h.Insert(Int(n))
+	}
+
+	ordered := make([]Int, 0)
+	for h.Len() > 0 {
+		ordered = append(ordered, h.Extract().(Int))
+	}
+
+	if nil != h.Extract() {
+		t.Error()
+	}
+
+	for i := 0; i < len(ordered)-2; i++ {
+		if ordered[i] < ordered[i+1] {
+			t.Errorf("!Ordered. %v", ordered)
+		}
+	}
+}
